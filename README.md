44b934c1622a1face93b53f98c33a443501654c8
Liuqi <goliuqi@gmail.com>
2020-10-22 09:46:54 +0800

    关联

--- README.md
+++ README.md
@@ -1,28 +1,24 @@
-# test
+# vue-learn
 
 ## Project setup
 ```
-npm install
+yarn install
 ```
 
 ### Compiles and hot-reloads for development
 ```
-npm run serve
+yarn serve
 ```
 
 ### Compiles and minifies for production
 ```
-npm run build
+yarn build
 ```
 
 ### Lints and fixes files
 ```
-npm run lint
+yarn lint
 ```
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
-
-## 临时笔记
-吃饼干
-1. 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './plugins/element.js'
 
 Vue.config.productionTip = false
+
+// Vue.component(Button.name, Button);
+// Vue.component(Select.name, Select);
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// import Vue from 'vue';
+// // import ElementUI from 'element-ui';
+// // import 'element-ui/lib/theme-chalk/index.css';
+// import App from './App.vue';
+
+// Vue.use(ElementUI);
+
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// });
